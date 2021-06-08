5c8fc3df8330492dc4c73231dc467d77639a5bb6
Pavan982003 <85146816+Pavan982003@users.noreply.github.com>
2021-06-08 07:51:06 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,20 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a=int(input())
+b=int(input())
+c=a+b
+d=a-b
+e=a*b
+f=a%b
+g=a/b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
